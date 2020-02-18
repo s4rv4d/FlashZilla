6adbfb430c088ff5c1438db95525b8385d5b56da
s4rv4d <sarvadshetty@gmail.com>
2020-02-18 21:16:12 +0530

    Initial Commit

---
+++ FlashZilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashZilla
+//
+//  Created by Sarvad shetty on 2/18/20.
+//  Copyright © 2020 Sarvad shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
